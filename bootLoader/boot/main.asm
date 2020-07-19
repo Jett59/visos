139c077e84ab30c2ea3cc87c685ced0a8ae08637
Luke Thompson <endeavour9@gmail.com>
2020-07-19 15:26:58 +1000

    attempt to write hello world without loading kernel

--- bootLoader/boot/main.asm
+++ bootLoader/boot/main.asm
@@ -1,57 +1,36 @@
 org 0x7C00
 bits 16
 
-;Note that the jump and NOP are part of the BPB
-jmp short start
-nop
-
-; The following code wasn't written by me
-; it's just a Standard BIOS Parameter Block with a FAT12/FAT16 extension
-; considering the comments are pretty good and already describe the use of each value
-; we might just use this as it's working (which is something I had many problems with).
-; Source MikeOS
-; http://mikeos.sourceforge.net/
-; ------------------------------------------------------------------
-; Disk description table, to make it a valid floppy
-; Note: some of these values are hard-coded in the source!
-; Values are those used by IBM for 1.44 MB, 3.5" diskette
-OEMLabel		db "Example "	; Disk label
-BytesPerSector		dw 512		; Bytes per sector
-SectorsPerCluster	db 1		; Sectors per cluster
-ReservedForBoot		dw 1		; Reserved sectors for boot record
-NumberOfFats		db 2		; Number of copies of the FAT
-RootDirEntries		dw 224		; Number of entries in root dir
-; (224 * 32 = 7168 = 14 sectors to read)
-LogicalSectors		dw 2880		; Number of logical sectors
-MediumByte		db 0F0h		; Medium descriptor byte
-SectorsPerFat		dw 9		; Sectors per FAT
-SectorsPerTrack		dw 18		; Sectors per track (36/cylinder)
-Sides			dw 2		; Number of sides/heads
-HiddenSectors		dd 0		; Number of hidden sectors
-LargeSectors		dd 0		; Number of LBA sectors
-; MikeOS's bootloader didn't mention this but the FAT12/FAT16 extension starts here
-DriveNo			dw 0		; Drive No: 0
-Signature		db 41		; Drive signature: 41 for floppy
-VolumeID		dd 00000000h	; Volume ID: any number
-VolumeLabel		db "Example    "; Volume Label: any 11 chars
-FileSystem		db "FAT12   "	; File system type: don't change!
-start: 
-; ------------------------------------------------------------------
-
 mov ah, 0
 int 0x13
 
-mov al, 1
-mov bx, 0x8000
-mov ch, 0
-mov dh, 0
-mov cl, 2
-mov ah, 2
-int 0x13
-jmp 0x8000
+mov si, msg
+call printNullTerminatedString
+
+printCharacter:
+	;before calling this function al must be set to the character to print
+	mov bh, 0x00 ;page to write to, page 0 is displayed by default
+	mov bl, 0x00 ;color attribute, doesn't matter for now
+	mov ah, 0x0E 
+	int 0x10 ; int 0x10, 0x0E = print character in al
+	ret	
+
+printNullTerminatedString:
+	pusha ;save all registers to be able to call this from where every we want
+	.loop:
+		lodsb ;loads byte from si into al and increases si
+		test al, al ;test if al is 0 which would mean the string reached it's end
+		jz .end
+		call printCharacter ;print character in al
+	jmp .loop ;print next character
+	.end:
+	popa ;restore registers to original state
+	ret
+
+msg db "Hello World!"
 
 ; Padding to 510 bytes with 0s
-times 510-($-$$) db 0
+pad times 510-($-$$) db 0
 
 ; Master Boot Record Signature
 db 0x55 ; byte 511 = 0x55
