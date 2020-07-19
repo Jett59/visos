cdac1d37d1c65d905071e8494f425be01f0cb1af
Luke Thompson <endeavour9@gmail.com>
2020-07-19 14:54:38 +1000

    add a block of assembly which lets us emulate a floppy disk

--- bootLoader/boot/main.asm
+++ bootLoader/boot/main.asm
@@ -1,7 +1,46 @@
 org 0x7C00
 bits 16
+
+;Note that the jump and NOP are part of the BPB
+jmp short start
+nop
+
+; The following code wasn't written by me
+; it's just a Standard BIOS Parameter Block with a FAT12/FAT16 extension
+; considering the comments are pretty good and already describe the use of each value
+; we might just use this as it's working (which is something I had many problems with).
+; Source MikeOS
+; http://mikeos.sourceforge.net/
+; ------------------------------------------------------------------
+; Disk description table, to make it a valid floppy
+; Note: some of these values are hard-coded in the source!
+; Values are those used by IBM for 1.44 MB, 3.5" diskette
+OEMLabel		db "Example "	; Disk label
+BytesPerSector		dw 512		; Bytes per sector
+SectorsPerCluster	db 1		; Sectors per cluster
+ReservedForBoot		dw 1		; Reserved sectors for boot record
+NumberOfFats		db 2		; Number of copies of the FAT
+RootDirEntries		dw 224		; Number of entries in root dir
+; (224 * 32 = 7168 = 14 sectors to read)
+LogicalSectors		dw 2880		; Number of logical sectors
+MediumByte		db 0F0h		; Medium descriptor byte
+SectorsPerFat		dw 9		; Sectors per FAT
+SectorsPerTrack		dw 18		; Sectors per track (36/cylinder)
+Sides			dw 2		; Number of sides/heads
+HiddenSectors		dd 0		; Number of hidden sectors
+LargeSectors		dd 0		; Number of LBA sectors
+; MikeOS's bootloader didn't mention this but the FAT12/FAT16 extension starts here
+DriveNo			dw 0		; Drive No: 0
+Signature		db 41		; Drive signature: 41 for floppy
+VolumeID		dd 00000000h	; Volume ID: any number
+VolumeLabel		db "Example    "; Volume Label: any 11 chars
+FileSystem		db "FAT12   "	; File system type: don't change!
+start: 
+; ------------------------------------------------------------------
+
 mov ah, 0
 int 0x13
+
 mov al, 1
 mov bx, 0x8000
 mov ch, 0
